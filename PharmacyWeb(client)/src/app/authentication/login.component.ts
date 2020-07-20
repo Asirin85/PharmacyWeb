9e656a2ef401d73c18a8ec6294c6203c56a22e05
Asirin85 <68087652+Asirin85@users.noreply.github.com>
2020-07-20 12:29:03 +0400

    Add files via upload

--- PharmacyWeb(client)/src/app/authentication/login.component.ts
+++ PharmacyWeb(client)/src/app/authentication/login.component.ts
@@ -18,14 +18,14 @@ export class LoginComponent {
   }
 
   login() {
-    this.success = false;
+
     this.auth.authenticate(this.credentials).subscribe((result) => {
-      if (result) {
         sessionStorage.setItem('username', this.credentials.username);
         sessionStorage.setItem('token', 'Basic ' + btoa(this.credentials.username + ':' + this.credentials.password));
         this.router.navigateByUrl('/');
         this.success = true;
-      }
+    },()=>{
+      this.success = false;
     });
 
   }
